8d1574144bc31dc7eff13dc2ecccacdb079f5162
Gleb <Glebocrew@mail.ru>
2020-11-08 11:40:53 +0300

    Done

--- matrix.py
+++ matrix.py
@@ -1,2 +1,22 @@
 # Calculate the sums of even and odd elemens of matrix
 
+from random import randint as rndnt
+
+width = 2
+height = 2
+Sum_even = 0
+Sum_odd = 0
+for x in range(0, height, 1):
+    Row = []
+    for y in range(0, width, 1):
+        N = rndnt(0, 100)
+        Row.append(N)
+        if N % 2 == 0:
+            Sum_even += N
+        else:
+            Sum_odd += N
+    for char in Row:
+        print(str(char) + " | ", end='')
+    print("\n")
+print("This is the result of odd elements : %d" % Sum_odd)
+print("This is the result of even elements : %d" % Sum_even)
